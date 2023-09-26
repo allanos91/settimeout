e911011777839ccb8bc83cfc4091ae33140e3758
Alexander <allanosinhb@gmail.com>
2023-09-25 20:53:39 -0700

    finished

--- problems/04-interval-count.js
+++ problems/04-interval-count.js
@@ -1,12 +1,12 @@
 /***********************************************************************
-Write a function, `intervalCount`, that accepts a callback, a delay in 
-milliseconds, and an amount. The function should set an interval with 
+Write a function, `intervalCount`, that accepts a callback, a delay in
+milliseconds, and an amount. The function should set an interval with
 the given callback and delay, but clear the interval after the callback
 has been executed 'amount' number of times.
 
 Hint: utilize a 'closure' to your advantage
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Example
@@ -14,10 +14,40 @@ Example
 intervalCount(function() {
     console.log('hi');
 }, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
-***********************************************************************/
+
+//setInterval(code)
+setInterval(code, delay)
+
+setInterval(func)
+setInterval(func, delay)
+setInterval(func, delay, arg1)
+// setInterval(func, delay, arg1, arg2)
+// setInterval(func, delay, arg1, arg2, /* …, */
+// // 7:43
+// // clearInterval(intervalID)
+// // 7:43
+// // setTimeout(code)
+// // setTimeout(code, delay)
+
+// // setTimeout(functionRef)
+// // setTimeout(functionRef, delay)
+// // setTimeout(functionRef, delay, param1)
+// // setTimeout(functionRef, delay, param1, param2)
+// // setTimeout(functionRef, delay, param1, param2, /* …, */ paramN)
+// // ***********************************************************************/
 
 function intervalCount(cb, delay, amount) {
-  // Your code here 
+  let id = setInterval(execute, delay)
+  let count = 0;
+
+  function execute() {
+    cb()
+    count++
+    if (count === amount) {
+      clearInterval(id)
+      id = null;
+    }
+  }
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
